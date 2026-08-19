fa66111d116704e456c3d2396f35ce9bf51da7a3
angellopez15luisa-hash <angellopez15luisa@gmail.com>
2026-08-19 05:56:35 -0500

    19-08-2026 commit 12

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,22 @@ const PORT = process.env.PORT || 4700
 const httpServer = http.createServer(server)
 
 // Configuramos CORS exclusivo para el protocolo de WebSockets
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: [process.env.FRONTEND_URL,process.env.FRONTEND_URL_LANDING,process.env.FRONTEND_URL_5174], // El puerto de tu frontend en Vue
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+//   }
+// });
 const io = new Server(httpServer, {
   cors: {
-    origin: [process.env.FRONTEND_URL,process.env.FRONTEND_URL_LANDING,process.env.FRONTEND_URL_5174], // El puerto de tu frontend en Vue
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  }
+    origin: [
+      'http://localhost:5173', // Para cuando desarrolles local
+      'https://landingpage-vue-frontend.vercel.app', // Tu Vercel actual
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  },
 });
 
 // 3. Escuchamos las conexiones de los clientes
