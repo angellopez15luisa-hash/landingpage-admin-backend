a48f18a83cbb1b552eb8fbc16e23804fc14be745
angellopez15luisa-hash <angellopez15luisa@gmail.com>
2026-08-19 04:32:25 -0500

    19-08-2026 commit 7

--- src/routes/order-step.route.ts
+++ src/routes/order-step.route.ts
@@ -2,8 +2,13 @@ import { Router } from "express";
 import { OrderStepController } from "../controllers";
 import { validateSchema } from "../middlewares/validate.middleware";
 import { OrderStepSchema } from "../schemas";
+import { UserMiddleware } from "../middlewares";
 
 const router = Router();
+
+router.get("/´public", OrderStepController.getAll);
+
+router.use(UserMiddleware.verifyToken);
 
 router.get("/", OrderStepController.getAll);
 
